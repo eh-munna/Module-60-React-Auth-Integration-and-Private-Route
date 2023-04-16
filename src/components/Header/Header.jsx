38e07aabc46c2d437e183ee999ac14cb0d80679d
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-04-17 00:55:56 +0200

    60-2 Create Login and Registration Form using Daisy UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <nav>
-      <div className="navbar bg-base-300">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      <div className="navbar bg-base-300 gap-6">
+        <a className="btn btn-ghost normal-case text-xl">Logo</a>
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,75 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import app from '../../firebase/firebase.config';
+import { getAuth } from 'firebase/auth';
+
+const auth = getAuth(app);
 
 const Register = () => {
-  return <div></div>;
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+  };
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Please Register</h1>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleRegister} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                required
+                name="name"
+                type="text"
+                placeholder="name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                required
+                name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                required
+                name="password"
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Register</button>
+            </div>
+          </form>
+          <div className="text-center mb-6">
+            Have an account?{' '}
+            <Link to="/login">
+              <button className="hover:underline text-[#570df8]"> Login</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
